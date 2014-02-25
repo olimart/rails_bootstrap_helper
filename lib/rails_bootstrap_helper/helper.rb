9e393b7569c69b6542d248f5770372ff8389f03b
Olivier <olivier.simart@gmail.com>
2014-02-25 01:20:58 -0500

    Icon button link to for BS 3.1

--- lib/rails_bootstrap_helper/helper.rb
+++ lib/rails_bootstrap_helper/helper.rb
@@ -1,11 +1,11 @@
 # coding: utf-8
 module RailsBootstrapHelper
-  
+
   module Helper
     def info_tag(text)
-      content_tag(:i, '', class: 'icon-info-sign', rel: 'tooltip', title: text) + " "
+      content_tag(:i, '', class: 'glyphicon-info-sign', rel: 'tooltip', title: text) + " "
     end
-    
+
     def status_tag(status, options = {})
       klass = ["label"]
       if options.has_key?(:level)
@@ -26,20 +26,25 @@ module RailsBootstrapHelper
       content_tag(:span, status, options)
     end
 
+    def badge_link_to(name, url, options={})
+      badge = content_tag(:span, options.delete(:count).presence || "", class: "badge")
+      link_to((name + " " + badge).html_safe, url, options)
+    end
+
     def icon_link_to(text, url, options)
       icon = options.delete(:icon) if options.has_key?(:icon)
       icon_tag = ''
       if icon
-        icon_klass = ["icon-#{icon}"]
+        icon_klass = ["glyphicon-#{icon}"]
         if options.has_key?(:white)
           icon_klass << ["icon-white"]
           options.delete(:white)
         end
-        icon_tag = content_tag(:i, '', :class => icon_klass.join(' '))
+        icon_tag = content_tag(:i, '', class: icon_klass.join(' '))
       end
       link_to (icon_tag + " #{text}").html_safe, url, options
     end
-    
+
     def button_link_to(text, url, options={})
       klass = ["btn"]
       if options.has_key?(:size)
@@ -54,96 +59,33 @@ module RailsBootstrapHelper
       options[:class] = klass.flatten.join(" ")
       link_to text, url, options
     end
-    
+
     def icon_button_link_to(text, url, options)
+      klass = ["btn"]
       icon = options.delete(:icon) if options.has_key?(:icon)
       icon_tag = ''
       if icon
-        klass = ["icon-#{icon}"]
+        klass = ["glyphicon glyphicon-#{icon}"]
         if options.has_key?(:white)
           klass << ["icon-white"]
           options.delete(:white)
         end
-        icon_tag = content_tag(:i, '', :class => klass.join(' '))
+        icon_tag = content_tag(:span, '', class: klass.join(' '))
       end
-      
+      if options.has_key?(:size)
+        klass << "btn-#{options[:size]}"
+        options.delete(:size)
+      end
+      if options.has_key?(:level)
+        klass << "btn-#{options[:level]}"
+        options.delete(:level)
+      end
+
+      klass << options[:class].strip.split(/\s+/) unless options[:class].blank?
+      options[:class] = klass.flatten.join(" ")
       button_link_to (icon_tag + " #{text}").html_safe, url, options
     end
 
-    def percent_input(f, field, lbl, input_field_options={})
-      if input_field_options[:info].present?
-        lbl = info_tag(input_field_options[:info]) + lbl
-        input_field_options.delete(:info)
-      end
-
-      append = input_field_options.delete(:append)
-      content = [content_tag(:span, '%', :class => "add-on"), " ", f.input_field(field, input_field_options)]
-      content.reverse! if append
-
-      f.input field, label: lbl, :wrapper => append ? :append : :prepend do
-        content.join().html_safe
-      end  
-    end
-
-    def money_input(f, field, lbl, input_field_options={})
-      if input_field_options[:info].present?
-        lbl = info_tag(input_field_options[:info]) + lbl
-        input_field_options.delete(:info)
-      end
-
-      append = input_field_options.delete(:append)
-      content = [content_tag(:span, '$', :class => "add-on"), " ", f.input_field(field, input_field_options)]
-      content.reverse! if append
-
-      f.input field, label: lbl, :wrapper => append ? :append : :prepend do
-        content.join().html_safe
-      end  
-    end
-
-    def date_input(f, field, lbl, input_field_options={})
-      if input_field_options[:info].present?
-        lbl = info_tag(input_field_options[:info]) + lbl
-        input_field_options.delete(:info)
-      end
-
-      append = input_field_options.delete(:append)
-      content = [content_tag(:span, content_tag(:i, '', class: 'icon-calendar'), :class => "add-on"), " ", f.input_field(field, {as: :string, class: "datepicker"}.merge(input_field_options))]
-      content.reverse! if append
-
-      f.input field, label: lbl, :wrapper => append ? :append : :prepend do
-        content.join().html_safe
-      end  
-    end
-
-    def url_input(f, field, lbl, input_field_options={})
-      if input_field_options[:info].present?
-        lbl = info_tag(input_field_options[:info]) + lbl
-        input_field_options.delete(:info)
-      end
-
-      append = input_field_options.delete(:append)
-      content = [content_tag(:span, 'http://', :class => "add-on"), " ", f.input_field(field, input_field_options)]
-      content.reverse! if append
-
-      f.input field, label: lbl, :wrapper => append ? :append : :prepend do
-        content.join().html_safe
-      end  
-    end
-
-    def user_input(f, field, lbl, input_field_options={})
-      if input_field_options[:info].present?
-        lbl = info_tag(input_field_options[:info]) + lbl
-        input_field_options.delete(:info)
-      end
-
-      append = input_field_options.delete(:append)
-      content = [content_tag(:span, content_tag(:i, '', class: 'icon-user'), :class => "add-on"), " ", f.input_field(field, input_field_options)]
-      content.reverse! if append
-
-      f.input field, label: lbl, :wrapper => append ? :append : :prepend do
-        content.join().html_safe
-      end  
-    end
   end
 
 end
